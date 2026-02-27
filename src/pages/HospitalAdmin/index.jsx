ad9f7dc98cf0acf6c4e4a79b1cea1b2813e9208e
shakibjilani <shakib.jilani@sakhaglobal.com>
2026-02-27 13:50:46 +0530

    Hospital

--- src/pages/HospitalAdmin/index.jsx
+++ src/pages/HospitalAdmin/index.jsx
@@ -3,8 +3,8 @@ import HospitalAdminLayout from '../../components/Layouts/HospitalAdminLayout'
 
 import Dashboard from './Dashboard'
 import Employee from './Employee/index'
-import Billing from './Billing'
-import Reports from './Reports'
+import Attendance from './Attendance/index'
+import LeaveTracker from './LeaveTracker/index'
 import Settings from './Settings'
 
 const AdminRoutes = () => {
@@ -13,10 +13,8 @@ const AdminRoutes = () => {
       <Route element={<HospitalAdminLayout />}>
         <Route path='dashboard' element={<Dashboard />} />
         <Route path='employee/*' element={<Employee />} />
-        {/* <Route path='attendance' element={<Attendance />} /> */}
-        {/* <Route path='leave-tracker' element={<LeaveTracker />} /> */}
-        <Route path='billing' element={<Billing />} />
-        <Route path='reports' element={<Reports />} />
+        <Route path='attendance/*' element={<Attendance />} />
+        <Route path='leave-tracker' element={<LeaveTracker />} />
         <Route path='settings' element={<Settings />} />
       </Route>
     </Routes>
